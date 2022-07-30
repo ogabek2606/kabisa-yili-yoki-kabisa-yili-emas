44c23fcaa8bd64ce91660ec9fd3027f8e9c2c1c4
ogabek2606 <110012533+ogabek2606@users.noreply.github.com>
2022-07-30 02:01:35 -0400

    Add files via upload

---
+++ muhim/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner ss = new Scanner(in);
+        int passwort =2009;
+        int nema = 1111;
+        int carta = 700000;
+        System.out.println("passwort witter ");
+        System.out.println("=");
+        int n = ss .nextInt();
+
+
+        if (passwort== n ){
+            System.out.println("50000 ->[1]");
+            System.out.println("100000  ->[2]");
+            System.out.println("200000  ->[3]");
+            System.out.println("250000  ->[4]");
+    }
+}}
